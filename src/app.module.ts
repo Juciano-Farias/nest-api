54845f6b853d895481d3134233dd1d2093f89c23
Juciano <juciano.junior@lais.huol.ufrn.br>
2023-03-21 13:24:15 +0000

    :wrench: chore: Prisma module global export

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+// This is the main module, which is going to import other modules
+
+// Module: It's a class with the @Module decorator (decorator is a function that adds metadata to the current class or function)
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
 })
 export class AppModule {}
